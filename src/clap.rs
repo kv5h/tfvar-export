57eb2613b2581069e02f823edca3523890de70e4
kv5h <107398371+kv5h@users.noreply.github.com>
2024-05-05 23:08:20 +0900

    Applied formatter

--- src/clap.rs
+++ src/clap.rs
@@ -1,53 +1,53 @@
 use clap::{crate_description, crate_name, crate_version, Arg, ArgAction, Command};
 
 pub fn new_clap_command() -> clap::ArgMatches {
-	Command::new(crate_name!())
-		.about(crate_description!())
-		.version(crate_version!())
-		.arg(
-			Arg::new("base_url")
-				.short('b')
-				.long("base-url")
-				.default_value("https://app.terraform.io/api/v2")
-				.require_equals(false)
-				.required(false)
-				.value_name("BASE_URL")
-				.help("Base URL of Terraform API"),
-		)
-		.arg(
-			Arg::new("enable_info_log")
-				.short('l')
-				.long("info-log")
-				.action(ArgAction::SetTrue)
-				.help("Enable `Info` log"),
-		)
-		.arg(
-			Arg::new("show_outputs")
-				.short('s')
-				.long("show-outputs")
-				.action(ArgAction::SetTrue)
-				.help("Show a list of outputs and exit"),
-		)
-		.arg(
-			Arg::new("allow_update")
-				.short('u')
-				.long("allow-update")
-				.action(ArgAction::SetTrue)
-				.help("Allow update of the value when a variable already exists"),
-		)
-		.arg(
-			Arg::new("tfstate_file")
-				.index(1)
-				.required(true)
-				.value_name("PATH_TO_TFSTATE_FILE")
-				.help("Path to tfstate file"),
-		)
-		.arg(
-			Arg::new("export_list")
-				.index(2)
-				.required(true)
-				.value_name("PATH_TO_EXPORT_LIST")
-				.help("Path to export list"),
-		)
-		.get_matches()
+    Command::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .arg(
+            Arg::new("base_url")
+                .short('b')
+                .long("base-url")
+                .default_value("https://app.terraform.io/api/v2")
+                .require_equals(false)
+                .required(false)
+                .value_name("BASE_URL")
+                .help("Base URL of Terraform API"),
+        )
+        .arg(
+            Arg::new("enable_info_log")
+                .short('l')
+                .long("info-log")
+                .action(ArgAction::SetTrue)
+                .help("Enable `Info` log"),
+        )
+        .arg(
+            Arg::new("show_outputs")
+                .short('s')
+                .long("show-outputs")
+                .action(ArgAction::SetTrue)
+                .help("Show a list of outputs and exit"),
+        )
+        .arg(
+            Arg::new("allow_update")
+                .short('u')
+                .long("allow-update")
+                .action(ArgAction::SetTrue)
+                .help("Allow update of the value when a variable already exists"),
+        )
+        .arg(
+            Arg::new("tfstate_file")
+                .index(1)
+                .required(true)
+                .value_name("PATH_TO_TFSTATE_FILE")
+                .help("Path to tfstate file"),
+        )
+        .arg(
+            Arg::new("export_list")
+                .index(2)
+                .required(true)
+                .value_name("PATH_TO_EXPORT_LIST")
+                .help("Path to export list"),
+        )
+        .get_matches()
 }
--- src/get_outputs.rs
+++ src/get_outputs.rs
@@ -5,8 +5,8 @@ use std::io::BufReader;
 
 /// Struct of output in tfstate
 pub struct TfstateOutput {
-	name: String,
-	value: serde_json::Value,
+    name: String,
+    value: serde_json::Value,
 }
 
 /// Get outputs from tfstate.
@@ -28,37 +28,37 @@ pub struct TfstateOutput {
 /// value: Number(0)
 /// ```
 pub fn get_outputs(
-	show_output: bool,
-	file_path: &str,
+    show_output: bool,
+    file_path: &str,
 ) -> Result<Vec<TfstateOutput>, Box<dyn std::error::Error>> {
-	let tfstate = std::fs::File::open(file_path)?;
-	let mut buf_reader = BufReader::new(tfstate);
-	let mut contents = String::new();
-	buf_reader.read_to_string(&mut contents)?;
+    let tfstate = std::fs::File::open(file_path)?;
+    let mut buf_reader = BufReader::new(tfstate);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
 
-	let contents_json: serde_json::Value = serde_json::from_str(&contents)?;
-	let tfstate_outputs: Vec<TfstateOutput> = contents_json["outputs"]
-		.as_object()
-		.unwrap()
-		.into_iter()
-		.map(|val| TfstateOutput {
-			name: val.0.to_string(),
-			value: val.1["value"].clone(),
-		})
-		.collect();
+    let contents_json: serde_json::Value = serde_json::from_str(&contents)?;
+    let tfstate_outputs: Vec<TfstateOutput> = contents_json["outputs"]
+        .as_object()
+        .unwrap()
+        .into_iter()
+        .map(|val| TfstateOutput {
+            name: val.0.to_string(),
+            value: val.1["value"].clone(),
+        })
+        .collect();
 
-	if show_output {
-		// Stdout
-		println!("Number of outputs: {}.", tfstate_outputs.len());
-		tfstate_outputs.iter().enumerate().for_each(|(idx, val)| {
-			println!(
-				"--- {} ---\nname : {}\nvalue: {:?}",
-				idx + 1,
-				val.name,
-				val.value
-			);
-		});
-	}
+    if show_output {
+        // Stdout
+        println!("Number of outputs: {}.", tfstate_outputs.len());
+        tfstate_outputs.iter().enumerate().for_each(|(idx, val)| {
+            println!(
+                "--- {} ---\nname : {}\nvalue: {:?}",
+                idx + 1,
+                val.name,
+                val.value
+            );
+        });
+    }
 
-	Ok(tfstate_outputs)
+    Ok(tfstate_outputs)
 }
